232226bb0b0d1d14af0f362d36217d54e3c1407c
oxford-arc <66992362+oxford-arc@users.noreply.github.com>
2020-07-02 14:11:32 +0100

    Updated

--- runbench.sh
+++ runbench.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
+#
+#
+# These should be configured as per the instructions:
+
+export GPUCOUNT=2
+export FP16=True
+
 
 # ARC Specifics
 #
@@ -21,18 +28,38 @@ git clone -b cnn_tf_v1.15_compatible  https://github.com/tensorflow/benchmarks
 export IMAGE=$PWD/image/tensorflow-19.12-tf1-py3.sif
 export BENCHDIR=$PWD/benchmarks/scripts/tf_cnn_benchmarks
 
+export OUTPUT=/tmp/run.out
+export RESULTS=${PWD}/results.txt
+
+echo "Output to $RESULTS."
+rm -f $RESULTS
+touch $RESULTS
+
 # Ensure we are in benchmark directory:
 #
 cd $BENCHDIR
+
+echo -n "Running.."
 #
-echo "---------------------------------------------------------------------------"
-echo "TAG Inception v3: "
-singularity exec --nv -B ${PWD}:/host_pwd --pwd /host_pwd $IMAGE python tf_cnn_benchmarks.py --num_gpus=1 --batch_size=32 --model=inception3 --use_fp16=True --variable_update=parameter_server
-echo "TAG VGG16: "
-singularity exec --nv -B ${PWD}:/host_pwd --pwd /host_pwd $IMAGE python tf_cnn_benchmarks.py --num_gpus=1 --batch_size=32 --model=vgg16 --use_fp16=True --variable_update=parameter_server
-echo "TAG ResNet50: "
-singularity exec --nv -B ${PWD}:/host_pwd --pwd /host_pwd $IMAGE python tf_cnn_benchmarks.py --num_gpus=1 --batch_size=32 --model=resnet50 --use_fp16=True --variable_update=parameter_server
-echo "TAG ResNet152: "
-singularity exec --nv -B ${PWD}:/host_pwd --pwd /host_pwd $IMAGE python tf_cnn_benchmarks.py --num_gpus=1 --batch_size=32 --model=resnet152 --use_fp16=True  --variable_update=parameter_server
+for gpu in `seq 1 ${GPUCOUNT}`;
+do
+        echo -n "."
+        echo "---------------------------------------------------------------------------" >> $RESULTS
+        echo -n "$gpu GPU(s):FP16=${FP16}:" >> $RESULTS
+        echo -n "Inception v3: " >> $RESULTS
+        singularity exec --nv -B ${PWD}:/host_pwd --pwd /host_pwd $IMAGE python tf_cnn_benchmarks.py --num_gpus="${gpu}" --batch_size=32 --model=inception3 --use_fp16="${FP16}" --variable_update=parameter_server > $OUTPUT 2>&1
+        cat $OUTPUT | grep -i "total images" >> $RESULTS
+        echo -n "VGG16: " >> $RESULTS
+        singularity exec --nv -B ${PWD}:/host_pwd --pwd /host_pwd $IMAGE python tf_cnn_benchmarks.py --num_gpus="${gpu}" --batch_size=32 --model=vgg16 --use_fp16="${FP16}" --variable_update=parameter_server  > $OUTPUT 2>&1
+        cat $OUTPUT | grep -i "total images" >> $RESULTS
+        echo -n "ResNet50: " >> $RESULTS
+        singularity exec --nv -B ${PWD}:/host_pwd --pwd /host_pwd $IMAGE python tf_cnn_benchmarks.py --num_gpus="${gpu}" --batch_size=32 --model=resnet50 --use_fp16="${FP16}" --variable_update=parameter_server > $OUTPUT 2>&1
+        cat $OUTPUT | grep -i "total images" >> $RESULTS
+        echo -n "ResNet152: " >> $RESULTS
+        singularity exec --nv -B ${PWD}:/host_pwd --pwd /host_pwd $IMAGE python tf_cnn_benchmarks.py --num_gpus="${gpu}" --batch_size=32 --model=resnet152 --use_fp16="${FP16}"  --variable_update=parameter_server > $OUTPUT 2>&1
+        cat $OUTPUT | grep -i "total images" >> $RESULTS
+        echo >> $RESULTS
+        echo "---------------------------------------------------------------------------" >> $RESULTS
+
+done
 echo
-echo "---------------------------------------------------------------------------"
